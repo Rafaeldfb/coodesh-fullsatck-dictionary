08434338c1cacefbaf8a6759fe299cddcc45eea1
Rafaeldfb <dfbrafa@hotmail.com>
2024-10-08 11:51:50 -0300

    frontend application port config

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,4 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  server: {
+    port: process.env.PORT || 3000, // Use PORT from .env or default to 3000
+  },
+};
 
 export default nextConfig;
