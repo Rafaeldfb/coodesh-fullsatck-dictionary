3ea7ea13208ac276b27a20844296832f7c783eeb
Rafaeldfb <dfbrafa@hotmail.com>
2024-10-08 12:34:40 -0300

    [fix] port config rollback and new config by package.json file

--- front-end/next.config.mjs
+++ front-end/next.config.mjs
@@ -1,12 +1,4 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  server: {
-    port: process.env.PORT || 3000, // Use PORT from .env or default to 3000
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
