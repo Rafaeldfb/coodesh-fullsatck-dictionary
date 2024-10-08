97f94871127966aa4bc778b8907591292a86f657
Rafaeldfb <dfbrafa@hotmail.com>
2024-10-08 10:56:08 -0300

    start dev of entries controller and routes

--- back-end/src/routes/entriesRoutes.ts
+++ back-end/src/routes/entriesRoutes.ts
@@ -1,16 +1,27 @@
-import { Request, Response, NextFunction, Router } from 'express';
-// import authMiddleware from '../middleware/auth';
+import { NextFunction, Request, Response, Router } from 'express';
+import { RequestJwt } from '../@types/express';
+import authMiddleware from '../middleware/auth';
+import { getWordsListed } from '../controllers/entriesController'
 // import { LoginController, LogoutController, RefreshSessionToken } from '../controllers/authController';
 // import { CreateUserController } from '../controllers/userControllers';
-// import { RequestJwt } from '../@types/express';
 // import { AuthResponse } from '../@types/http';
 
 const router = Router();
 
 // Retornar a lista de palavras do dicionário, com paginação e suporte a busca. O endpoint de paginação de uma busca hipotética deve retornar a seguinte estrutura:
 // [GET]/entries/en?search=fire&limit=4
-router.get('/en', function(req: Request, res: Response) {
-  res.send({ message: 'under develepment - ' + '/en'});
+router.get('/en', async (req: RequestJwt, res: Response, next: NextFunction) => {
+  authMiddleware(req, res, next)
+}, async (req: Request, res: Response) => {
+  try {
+    const wordsListed = await getWordsListed(req, res);
+    res.status(200).json(wordsListed);
+
+  } catch (err) {
+    console.error('/en words list error:', err);
+
+    res.status(500).json({ message: "Under develepment" });
+  }
 });
 
 // Return the information for the specified word and record the access history.
